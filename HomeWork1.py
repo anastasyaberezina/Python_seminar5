5626ba0e463bf6fa4142c5154a1ce5a528fc6f8f
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-12-15 11:31:23 +0300

    Напишите программу, удаляющую из текста все слова, содержащие абв

---
+++ HomeWork1.py
@@ -0,0 +1,11 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+text = input('Введите текст: ')
+
+def ABC(text):
+
+    text = [i for i in text.split() if 'абв' not in i]
+    return ' '.join(text)
+
+new_text = ABC(text)    
+print(new_text)
